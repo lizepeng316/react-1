69fe328edc20f80d29885d917b8bc157035e41bd
lizepeng316 <62992915+lizepeng316@users.noreply.github.com>
2020-04-03 16:23:36 +0800

    aaa

--- my-app/src/todolist/app.js
+++ my-app/src/todolist/app.js
@@ -5,71 +5,66 @@ class App extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-        data : [],
-        showData:[],
-        input : '',
-        search : ''
-        };
+            data : [],
+            showData:[],
+            input : '',
+            search : ''
         }
+    }
     // 添加数据
     addData = () => {
-    let {data,showData,input}  =this.state
-    data.push(input)
-    showData = data
-    this.setState({
-        data:data,
-        showData:showData})
+        let {data,showData,input}  =this.state
+        data.push(input)
+        showData = data
+        this.setState({
+            data:data,
+            showData:showData
+        })
     }
     // 删除数据
     delData = (index) => {
-        let {data} = this.state
-        data.splice(index,1)
-        this.setState({data:data})
-    }
+            let {data} = this.state
+            data.splice(index,1)
+            this.setState({data:data})
+        }
     // 搜索数据
     search =() => {
         const arr = this.state.showData.filter((item,i)=>{
-        return item === this.state.search
-        })
-        if(arr.length === 0){
-            alert('未添加')
-        }else{
-        this.setState({
-            showData:arr
-        })
-        } 
+            return item === this.state.search
+            })
+            if(arr.length === 0){
+                alert('未添加')
+            }
+            else{
+                this.setState({showData:arr})
+            } 
     }
     // 返回原列表
     backData = () => {
-        this.setState({showData : this.state.data})
-    }
+            this.setState({showData : this.state.data})
+        }
 
     onChange = (i) => {
         if(i.target.className === "add"){
-        this.setState({
-            input:i.target.value
-        })
-        }else if(i.target.className === "search"){
-        this.setState({
-            search:i.target.value
-        })
+                this.setState({input:i.target.value})
+            }
+        else if(i.target.className === "search"){
+            this.setState({search:i.target.value})
+            }
         }
-        
-    }
     render(){
         return(
-        <div className = 'App'>
-            <h2 className = 'title'>TodoList和Search</h2>
-            <input className='add' onChange={this.onChange}></input>
-            <button onClick={this.addData}>add</button>
-            <input className='search' onChange={this.onChange}></input>
-            <button onClick={this.search}>search</button>
-            <button onClick={this.backData}>back</button>
-            {todoList(this.state.showData,this.delData,this.backData)}
-        </div>
-        )
+            <div className = 'App'>
+                <h2 className = 'title'>TodoList和Search</h2>
+                <input className='add' onChange={this.onChange}></input>
+                <button onClick={this.addData}>add</button>
+                <input className='search' onChange={this.onChange}></input>
+                <button onClick={this.search}>search</button>
+                <button onClick={this.backData}>back</button>
+                {todoList(this.state.showData,this.delData,this.backData)}
+            </div>
+            )
+        }
     }
-
-}
 
 export default App;
