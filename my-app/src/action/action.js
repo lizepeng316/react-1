3cce6bb714e1a55026dd7a5bcc6810980216d704
lizepeng316 <62992915+lizepeng316@users.noreply.github.com>
2020-04-10 16:08:31 +0800

    第二次修改

--- my-app/src/action/action.js
+++ my-app/src/action/action.js
@@ -3,45 +3,71 @@
 
 // 添加数据action 
 let textId = 0
-export const addData = listname => {
-    // console.log(123)
-    return {
-        type:addDataList,
-        id:textId ++,
-        listname
+export function addData () {
+    return (dispatch,getState) =>    
+        {let {listname} = getState().addDataList;
+        toDoLis.push(() =>{
+            dispatch({
+                type:addDataList,
+                id:textId ++,
+                listname})
+            })        
+        }
     }
-}
 // 删除数据action 
-export const delData = index => {
-    return {
-        type:delDataList,
-        index
+export function delData() {
+    return (dispatch,getState) => 
+        {let {index} = getState().delDataList;
+        state.toDoList.filter((item)=>{
+            return item.id !== action.id});
+        dispatch({
+            type:delDataList,
+            index})
+        }
     }
-}
 // 查找数据action 
-export const  search =() => {
-    return {
-        type:searchList,
-        listname,
-        index
+export function  search() {
+    return (dispatch,getState) => 
+        {let {listname} = getState().searchList;
+        state.showList.filter((item,i) => {
+            if(item.listname !== action.listname){
+                return alert('未添加')
+            }               
+            return state.toDoList.listname});
+        dispatch ({
+            type:searchList,
+            listname,
+            index})
+        }
     }
-}
 // 返回数据action 
-export const  backData = listname => {
-    return {
-        type:backDataList,
-        listname
+export function  backData () {
+    return (dispatch,getState)=>
+    {let {listname}= getState().backDataList;
+        dispatch
+        ({type:backDataList,
+        listname})
     }
 }
-export const onChange =(e)=>{
-    return{
-        type:listInput,
-        value:e.target.value
+// input 输入
+export function onChange (){
+    return (dispatch,getState) => 
+        {let {e} = getState().searchList;
+        if(e.target.className === "add"){
+            getState({input:e.target.value})
+        }
+        else if(e.target.className === "search"){
+            getState({search:e.target.value})
+            };
+        dispatch({
+            type:listInput,
+            value:e.target.value})
+        }
     }
-}
 
-export const addDataList = 'addDataList'
-export const delDataList = 'delDataList'
-export const searchList = 'searchList'
-export const backDataList = 'backDataList'
-export const listInput = 'listInput'
+const ns = namespace('appRedux')
+export const addDataList = ns( 'addDataList')
+export const delDataList = ns ('delDataList')
+export const searchList = ns ('searchList' ) 
+export const backDataList = ns ('backDataList')
+export const listInput = ns ('listInput')
--- my-app/src/action/reducer.js
+++ my-app/src/action/reducer.js
@@ -5,49 +5,37 @@ import *as actions from './action'
 const initialState = {
     toDoList:[],
     showList:[],
-    input:''
+    data:[],
+    input:'',
+    id : '',
+    listname: ''
 }
 
 // reducer类
 const appReducer = (state = initialState ,action) => {
-    // console.log(action.type)
     switch(action.type){
         // 添加数据
-        case actions.addDataList :{
-            const arr1 = Object.assign({}, state, { toDoList: [...state.listname, state.id], id:textId ++ });
-            return arr1
-        }    
+            case actions.addDataList :{ 
+                return  Object.assign({}, state, { toDoList: [...state.listname, state.id], id:textId ++ });
+                }    
         // 删除数据
-        case actions.delDataList :{
-            const arr2 = state.toDoList.filter((item)=>{
-                return item.id !== action.id});
-            return Object.assign({},sate,{id : arr2[action.id],index : action.index})
-        }
+            case actions.delDataList :{
+                return Object.assign({},sate,{id : action.id,index : action.index})
+                }
         // 查找数据
-        case actions.searchList :{
-            const arr3 =state.showList.filter((item,i) => {
-                if(item.listname !== action.listname){
-                    return alert('未添加')
-                }               
-                return state.toDoList.listname
-            });
-            return  Object.assign({},sate,{listname : arr3[action.listname]},{showList : [...sate.index]})
-        }
+            case actions.searchList :{
+                return  Object.assign({},sate,{listname : action.listname},{showList : [...sate.index]})
+                }
         // 返回数据
-        case actions.backDataList :{
-            const arr4 = Object.assign({showList : [...sate.listname]}, state, { toDoList: [...state.listname, state.id], id:textId ++ });
-            return arr4
-        }
+            case actions.backDataList :{ 
+                return Object.assign({showList : [...sate.listname]}, state, { toDoList: [...state.listname, state.id], id:textId ++ });
+                }
         // input输入
-        case actions.listInput :{
-            if(e.target.className === "add"){Object.assign({},state, {input:e.action.value})
-            }
-            else if(e.target.className === "search"){Object.assign({},state, {search:e.action.value})
-            };
-            return [...state.input.value]
+            case actions.listInput :{
+                return Object.assign({},state, {input:e.action.value},{search:e.action.value})
+                }
+            default :
+                return state;
         }
-        default :
-            return state;
     }
-}
 export {appReducer}
